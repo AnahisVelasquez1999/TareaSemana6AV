367255d99e859b4601d10af36fe44b787af63472
ANAI <ANAI@DESKTOP-FOQ2ME0>
2022-05-20 21:13:32 -0500

    Agregar archivos de proyecto.

---
+++ TareaSemana6AV/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TareaSemana6AV.Ws;
+using Xamarin.Forms;
+
+namespace TareaSemana6AV
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string Url = "http://192.168.1.10/moviles/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<TareaSemana6AV.Ws.Datos> _post;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGet_Clicked(object sender, EventArgs e)
+        {
+            var content = await client.GetStringAsync(Url);
+            List<TareaSemana6AV.Ws.Datos> posts = JsonConvert.DeserializeObject<List<TareaSemana6AV.Ws.Datos>>(content);
+            _post = new ObservableCollection<TareaSemana6AV.Ws.Datos>(posts);
+
+            MLSView.ItemsSource = _post;
+        }
+
+        private async void btnInsertar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new Post());
+        }
+
+        private void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void MyListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            ListView lv = (ListView)sender;
+            TareaSemana6AV.Ws.Datos club = (TareaSemana6AV.Ws.Datos)lv.SelectedItem;
+            await Navigation.PushModalAsync(new Put(club.codigo, club.nombre, club.apellido, club.edad));
+        }
+    }
+}
---
+++ TareaSemana6AV/Put.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using TareaSemana6AV.Ws;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TareaSemana6AV
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Put : ContentPage
+    {
+
+        public Put(int codigo, string nombre, string apellido, int edad)
+        {
+            InitializeComponent();
+            txtEdad.Text = Convert.ToString(edad);
+            txtCodigo.Text = Convert.ToString(codigo);
+            txtNombre.Text = nombre;
+            txtApellido.Text = apellido;
+        }
+        private async void btnRegresar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new MainPage());
+        }
+
+        private async void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+
+                parametros.Add("codigo", txtCodigo.Text);
+                parametros.Add("nombre", txtNombre.Text);
+                parametros.Add("apellido", txtApellido.Text);
+                parametros.Add("edad", txtEdad.Text);
+                cliente.UploadValues("http://192.168.1.10/moviles/post.php?codigo=" + txtCodigo.Text + "&nombre=" + txtNombre.Text + "&apellido=" + txtApellido.Text + "&edad=" + txtEdad.Text, "PUT", parametros);
+                await DisplayAlert("Warning", "Dato Actualizado correctamente", "ok");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("alerta", "Error: " + ex.Message, "ok");
+            }
+        }
+
+        private async void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                var parametros = new System.Collections.Specialized.NameValueCollection();
+                parametros.Remove(txtCodigo.Text);
+                parametros.Remove(txtNombre.Text);
+                parametros.Remove(txtApellido.Text);
+                parametros.Remove(txtEdad.Text);
+                cliente.UploadValues("http://192.168.1.10/moviles/post.php?codigo=" + txtCodigo.Text + "&nombre=" + txtNombre.Text + "&apellido=" + txtApellido.Text + "&edad=" + txtEdad.Text, "DELETE", parametros);
+                await DisplayAlert("Warning", "Dato Eliminado correctamente", "ok");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("alerta", "Error: " + ex.Message, "ok");
+            }
+        }
+    }
+}
